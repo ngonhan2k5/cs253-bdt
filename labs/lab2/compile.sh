67951eb299d378735d0c285bb5ecb0c089749786
root <root@dbt-lab.us-central1-a.c.rugged-memory-271901.internal>
2020-03-26 15:49:33 +0000

    submit lab2

---
+++ labs/lab2/compile.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+javac -classpath /usr/java/jdk1.7.0_67-cloudera/jre/lib/*:/usr/java/jdk1.7.0_67-cloudera/jre/lib/ext/*:/usr/lib/hadoop/client-0.20/*:/usr/lib/hadoop/*:/usr/lib/hadoop/lib/*:. WordCount.java
